6ed57fdb095046cb6d673201e60a29203ecce366
amjayem <csejayem19@gmail.com>
2024-02-29 23:15:13 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes/routes')
+const port = process.env.PORT || 8000
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// app.use('/', routes)
+app.get('/', (req, res) => {
+  res.send(`Server Running on port:${port} `)
+})
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`)
+})
