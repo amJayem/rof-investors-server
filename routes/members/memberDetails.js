3a0f5d7d364226a5b925f1ee91e8c3d2a846c2cd
amjayem <csejayem19@gmail.com>
2024-04-01 23:31:07 +0600

    get balance history of member by email

--- routes/members/memberDetails.js
+++ routes/members/memberDetails.js
@@ -1,13 +1,18 @@
 const membersModel = require('../../models/membersModel')
+const balanceHistoryModel = require('../../models/balanceHistoryModel')
 
 const membersDetails = async (req, res) => {
   try {
     const id = req.params.id
 
-    const findQuery = membersModel.findById({ _id: id })
-    const member = await findQuery.sort({ _id: -1 })
+    const member = await membersModel.findById({ _id: id })
+    // const member = await findQuery.sort({ _id: -1 })
 
-    res.status(200).json({ status: 200, member })
+    const balanceHistory = await balanceHistoryModel.find({
+      email: member?.email
+    })
+
+    res.status(200).json({ status: 200, member, balanceHistory })
   } catch (error) {
     // console.error(error)
     res.status(500).send({ message: error.message })
